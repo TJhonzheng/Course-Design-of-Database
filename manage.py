12a0f7ba2364d4327f97f9d053b7d022377d147e
JieHeng <zjh_will@qq.com>
2019-08-26 19:55:52 +0800

    Update manage.py
    添加测试内容

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+"sdssdsdsdsdsdssdsdsd"
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProj_1.settings')
